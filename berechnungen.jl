87c93dad67cfb69b0536436c381a9a36f37471d5
Hannah Penz <hpenz@diw.de>
2022-07-01 11:17:28 +0200

    updated berechnungen.jl und test_berechnungen.jl

--- berechnungen.jl
+++ berechnungen.jl
@@ -3,30 +3,36 @@ using DelimitedFiles
 @doc """
 Berechnet die Parameter w und b und gibt diese zurueck.
 """
-function calculate_params(alpha::VT, l::MT) where {T, VT<:AbstractVector{T}, MT<:AbstractMatrix{T}}
-    X=l[:,1:2]  #Teilmatrix der Spalten 1-2
-    Y=l[:,3]
-    k=length(Y)
-    w = alpha[1]*Y[1]*X[1:1,:]
-    #X[i:i,:] Zeilenvektor in Zeile i
-    for i=2:k
-        prod1 = alpha[i]*Y[i]*X[i:i,:]
-        w = w+prod1
+function calculate_params(alpha::Vector, l::Matrix)
+    if size(alpha)[1]!=size(l)[1]
+        throw(DomainError(alpha, "falsche Dimension"))
+    else
+        X=l[:,1:2]  #Teilmatrix der Spalten 1-2
+        Y=l[:,3]
+        k=size(Y)[1]
+        w = alpha[1]*Y[1]*X[1:1,:]
+        #X[i:i,:] Zeilenvektor in Zeile i
+        for i=2:k[1]
+            prod1 = alpha[i]*Y[i]*X[i:i,:]
+            w = w+prod1
+        end
+
+        A=[alpha l]     #schreibe Liste und alpha in selbe Matrix
+        l = size(A)[1]
+        b=0
+        for i=1:l
+            c=0
+            for j =1:l
+                if A[j,1]>10^-5
+                    c+=A[j,1]*A[j,4]*A[j,2:3]'*A[i,2:3]
+                end
+            end
+            b+=Y[i]-c
+        end
+        b=b/l
+        return w, b
     end
-
-    A=[alpha l]     #schreibe Liste und alpha in selbe Matrix
-    A=[A[:,1] .>10^(-5), :]      #filtert nach alphawerten >0 in erster Spalte.
-    l = length(A)
-    b=0
-    for i=1:l
-        c=0
-        for j =1:l
-            c+=A[1,j]*A[4,j]*A[2:3,j]'*A[2:3,i]
-        b+=Y[i]-c
-    b=b/l
-    return w, b
 end
-
 #- filename =problem_{i}_param.txt`
 @doc """
 Speichert die Parameter w und b als .txt Datei.
@@ -36,16 +42,5 @@ function save_params_to_file(w, b, filename::String)
     writedlm(filename, param)
 end
 
-#Schreibe Testfälle: 
-#1) Randfall mit keiner Eingabe -> soll Fehlermeldung werfen
-#2) Fall mit falschem Format -> soll Fehlermeldung werfen (Julia-fkt die Dimension der Datentypen zusammenpasst)
-#2) Fälle mit einem, zwei und drei Punkten und dann von Hand ausrechnen!
-
-
-
-#a = [1; 2; 1; 4]
-#A = [1 2 1; 1 2 -1; 1 2 -1; 1 2 1]
-
-#calculate_params(a,A)
 
 #save_params_to_file([2 4], -20, "/Users/Hannah/Desktop/Coma_Python_Ha/Coma_II/Beispiel.txt")
--- test/test_berechnungen.jl
+++ test/test_berechnungen.jl
@@ -1,6 +1,13 @@
 include("../berechnungen.jl")
 using Test
 
-@test calculate_params([1], [0 1 1]) == [0 1], 0
+@test calculate_params([1], [0 1 1])== ([0 1], 0)
+@test calculate_params([1; 1], [1 0 1; 0 1 -1])== ([1 -1], 0)
+@test calculate_params([1; 1; 2], [0 0 1; 0 1 1; 1 0.5 -1])== ([-2 0], 1)
+@test calculate_params([1; 0; 1], [0 0 1; 0 1 1; 1 -1 -1])==([-1 1], 0.6666666666666666)
+#@test calculate_params([1], [0 1 1; 0 1 1])== DomainError
 
-# @test calculate_params([1, 1], [0 1 1, 1 1 -1])
+#Testfälle: 
+#1) Randfall mit keiner Eingabe -> soll Fehlermeldung werfen
+#2) Fall mit falschem Format -> soll Fehlermeldung werfen (Julia-fkt die Dimension der Datentypen zusammenpasst)
+#2) Fälle mit einem, zwei und drei Punkten und dann von Hand ausrechnen!
