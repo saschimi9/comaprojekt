7edb79e455c2deb457237b9eebe8413c679c2890
Sascha Petznick <s.petznick@tu-berlin.de>
2022-07-03 18:38:08 +0200

    New branch for fix.

--- berechnungen.jl
+++ berechnungen.jl
@@ -3,40 +3,33 @@ using DelimitedFiles
 @doc """
 Berechnet die Parameter w und b und gibt diese zurueck.
 """
-function calculate_params(alpha::Vector, l::Matrix)
-    if size(alpha)[1]!=size(l)[1]
-        throw(DomainError(alpha, "falsche Dimension"))
-    else
-        X=l[:,1:2]  #Teilmatrix der Spalten 1-2
-        Y=l[:,3]
-        k=size(Y)[1]
-        w = alpha[1]*Y[1]*X[1:1,:]
-        #X[i:i,:] Zeilenvektor in Zeile i
-        for i=2:k[1]
-            prod1 = alpha[i]*Y[i]*X[i:i,:]
-            w = w+prod1
-        end
-
-        A=[alpha l]     #schreibe Liste und alpha in selbe Matrix
-        s = size(A)[1]
-        counter=0       #Zähler für richtige size
-        b=0
-        for i=1:s
-            c=0
-            if A[i,1]>10^-5
-                for j =1:s
-                    if A[j,1]>10^-5
-                        c+=A[j,1]*A[j,4]*A[j,2:3]'*A[i,2:3]
-                    end
-                end
-                b+=Y[i]-c
-                counter+=1
-            end
-        end
-        b=b/counter
-        return w, b
+function calculate_params(α::VT, X::MT) where {T, VT<:AbstractVector{T}, MT<:AbstractMatrix{T}}
+    x=X[:,1:2]  #teilmatrix der Spalten 1-2
+    y=X[:,3]
+    k=size(y)[1]
+    w = α[1]*y[1]*x[1,:]
+    #X[i:i,:] Zeilenvektor in Zeile i
+    for i=2:k
+        prod1 = α[i]*y[i]*x[i,:]
+        w = w + prod1
     end
+
+    A=[α X]     #schreibe Liste und alpha in selbe Matrix
+    A=A[A[:,1] .>10^(-5), :]      #filtert nur alpha Werte >0 in erster Spalte.
+    l = size(A)[1]
+    b=0
+    for i=1:l
+        c=0
+        for j =1:l
+            c += A[j,1]*A[j,4]*A[j,2:3]'*A[i,2:3]
+        end
+        b+=y[i]-c
+    end
+    b=b/l
+    return w, b
 end
+
+#Fragen: passt das Format der .txt Datei?
 #- filename =problem_{i}_param.txt`
 @doc """
 Speichert die Parameter w und b als .txt Datei.
@@ -45,5 +38,17 @@ function save_params_to_file(w, b, filename::String)
     param =[w, b]
     writedlm(filename, param)
 end
+
+#Schreibe Testfälle:
+#1) Randfall mit keiner Eingabe -> soll Fehlermeldung werfen
+#2) Fall mit falschem Format -> soll Fehlermeldung werfen (Julia-fkt die Dimension der Datentypen zusammenpasst)
+#2) Fälle mit einem, zwei und drei Punkten und dann von Hand ausrechnen!
+
+
+
+#a = [1; 2; 1; 4]
+#A = [1 2 1; 1 2 -1; 1 2 -1; 1 2 1]
+
+#calculate_params(a,A)
 
 #save_params_to_file([2 4], -20, "/Users/Hannah/Desktop/Coma_Python_Ha/Coma_II/Beispiel.txt")
--- test/test_berechnungen.jl
+++ test/test_berechnungen.jl
@@ -1,14 +1,14 @@
 include("../berechnungen.jl")
 using Test
 
-@test calculate_params([1], [0 1 1])== ([0 1], 0)
-@test calculate_params([1; 1], [1 0 1; 0 1 -1])== ([1 -1], 0)
-@test calculate_params([-1; 1], [1 0 1; 0 1 -1]) ==([-1 -1],0)
-@test calculate_params([1; 1; 2], [0 0 1; 0 1 1; 1 0.5 -1])== ([-2 0], 1)
-@test calculate_params([1; 0; 1], [0 0 1; 0 1 1; 1 -1 -1])==([-1 1], 1)
+@test calculate_params([1], [0 1 1])== ([0, 1], 0)
+@test calculate_params([1; 1], [1 0 1; 0 1 -1])== ([1, -1], 0)
+@test calculate_params([-1; 1], [1 0 1; 0 1 -1]) == ([-1, -1], 0)
+@test calculate_params([1; 1; 2], [0 0 1; 0 1 1; 1 0.5 -1]) == ([-2, 0], 1)
+@test calculate_params([1; 0; 1], [0 0 1; 0 1 1; 1 -1 -1]) ==([-1, 1], 1)
 #@test calculate_params([1], [0 1 1; 0 1 1])== DomainError
 
-#Testfälle: 
+#Testfälle:
 #1) Randfall mit keiner Eingabe -> soll Fehlermeldung werfen
 #2) Fall mit falschem Format -> soll Fehlermeldung werfen (Julia-fkt die Dimension der Datentypen zusammenpasst)
 #2) Fälle mit einem, zwei und drei Punkten und dann von Hand ausrechnen!
