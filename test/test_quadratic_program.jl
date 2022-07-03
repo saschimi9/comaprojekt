4e149ed6efdee48c9b9ebda682f4d5606fbf6445
SwimmyDan <108135667+SwimmyDan@users.noreply.github.com>
2022-07-03 15:00:52 +0200

    update quadratic_program.jl and test_quadratic_program.jl
    raised errors added to code plus appropriate tests in test file

--- test/test_quadratic_program.jl
+++ test/test_quadratic_program.jl
@@ -15,18 +15,26 @@ bsp2 = [3.0 1.0 1
 6.0 4.0 -1
 4.0 6.0 -1]
 
-#zwei weitere Beispiele
+#zwei weitere gueltige Beispiele
 horiz_line = [1 1 1
 1 0 -1]
 
 verti_line = [1 1 1
 0 1 -1]
 
+#zwei Beispiele mit ungueltige Dimension bzw. Eintraege
+dimension_error = [1 -1 1 1
+-1 1 -1 -1]
+
+entry_error = [1 -1 2]
+
 #jedes Beispiel getestet mit Fehler <10^-4
 @test isapprox(quadratic_program(bsp1), [0.5, 0.0, 0.5, 0.5, 0.0, 0.5]; atol = 10^-4)
 @test isapprox(quadratic_program(bsp2), [0.125, 0.125, 0.25, 0.0, 0.0]; atol = 10^-4)
 @test isapprox(quadratic_program(horiz_line), [2.0, 2.0]; atol = 10^-4)
 @test isapprox(quadratic_program(verti_line), [2.0, 2.0]; atol = 10^-4)
+@test_throws ErrorException quadratic_program(domain_error)
+@test_throws ErrorException quadratic_program(entry_error)
 
 #include("quadratic_program.jl")
 #include("berechnungen.jl")
