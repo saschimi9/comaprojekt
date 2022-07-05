dcec3cb5436ab68b18548c6ee4d69d29390156cb
Sascha Petznick <s.petznick@tu-berlin.de>
2022-07-05 15:26:45 +0200

    Fix julia main, remove main in Python, and update visualize.py.

--- main.jl
+++ main.jl
@@ -1,7 +1,5 @@
 using Pkg
 Pkg.add("QPDAS")
-Pkg.add("PyCall")
-Pkg.add("Conda")
 
 include("quadratic_program.jl")
 include("daten_einlesen.jl")
@@ -9,20 +7,12 @@ include("berechnungen.jl")
 
 dir = @__DIR__
 dir *= "/"
-filename = "Beispiel_2.txt"
+filename = "Beispiel_1.txt"
 params_filename = "params_" * filename
 X = read_points(dir*filename)
 α = quadratic_program(X)
 w, b = calculate_params(α, X)
 save_params_to_file(w, b, params_filename)
 
-# # Call to Python
-# ENV["PYTHON"] = ""
-# Pkg.build("PyCall")
-# using PyCall
-# using Conda
-# Conda.add("numpy")
-# Conda.add("matplotlib")
-# pybin = Conda.PYTHONDIR
-# # run(`source $dir.venv/bin/activate`)
-# run(`$pybin visualize.py $filename`)
+# Call to Python
+run(`$dir.venv/bin/python visualize.py $filename`)
