31b69b688ad83232dae35ad001c0778b046052ac
Sascha Petznick <s.petznick@tu-berlin.de>
2022-07-05 17:33:30 +0200

    Add example, update main for compatibility.

--- main.jl
+++ main.jl
@@ -6,13 +6,12 @@ include("daten_einlesen.jl")
 include("berechnungen.jl")
 
 dir = @__DIR__
-dir *= "/"
-filename = "Beispiel_1.txt"
+filename = ARGS[1]
 params_filename = "params_" * filename
-X = read_points(dir*filename)
+X = read_points(dir*"/"*filename)
 α = quadratic_program(X)
 w, b = calculate_params(α, X)
 save_params_to_file(w, b, params_filename)
 
 # Call to Python
-run(`$dir.venv/bin/python visualize.py $filename`)
+run(`$dir/venv/bin/python visualize.py $filename`)
